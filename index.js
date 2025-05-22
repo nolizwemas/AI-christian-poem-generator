e99faba15e1ca0bf90ef1b1bfa20c6f49df02d98
nolizwemas <nolizwemasondo@gmail.com>
2025-05-22 17:51:41 +0200

    Added the basic form handling and added typewriter plugin.

---
+++ index.js
@@ -0,0 +1,14 @@
+function generatePoem(event) {
+    event.preventDefault();
+
+    new Typewriter('#poem', {
+        strings: "The poem will go here",
+        autoStart: true,
+        delay: 1,
+        cursor: "",
+    });
+}
+
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
