2e9ff1044d267a685ac9afca5028cdab14dc9d59
nolizwemas <nolizwemasondo@gmail.com>
2025-05-23 19:13:13 +0200

    improved user experience

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+
+function displayPoem(response) {
+
+ new Typewriter('#poem', {
+     strings: response.data.answer,
+     autoStart: true,
+     delay: 1,
+     cursor: ""
+});   
+}
+
 function generatePoem(event) {
     event.preventDefault();
 
-    new Typewriter('#poem', {
-        strings: "The poem will go here",
-        autoStart: true,
-        delay: 1,
-        cursor: "",
-    });
+     let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "4d50fa0b4fo9407f4bc4tc0a244ca33d";
+    let context = "You are a very poetic AI assistant who provides the most beautiful 150 word Christian poems about topics in the Holy Bible";
+    let prompt = `User instructions: Generate a Christian poem ${instructionsInput.value}`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+    
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⏳ Generating a Christian poem about ${instructionsInput.value}</div>`;
+
+    axios.get(apiUrl).then(displayPoem);
+
+   
 }
 
 
